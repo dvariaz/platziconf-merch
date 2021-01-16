7a190b849181a9e5c38da675d338f1936c3f7713
David Arias <davidarias.devs@gmail.com>
2021-01-15 22:21:34 -0500

    Trying to set environment variables in github actions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './styles/libs/tailwind.css';
 import './styles/App.scss';
 
 import App from './routes/App';
+
+console.log(process.NODE_ENV);
 
 ReactDOM.render(<App />, document.getElementById('app'));
